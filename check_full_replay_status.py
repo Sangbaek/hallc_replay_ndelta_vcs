66f631efbce4bc34e86d98f151bb6400ea68b62c
Sangbeak Lee <sangbaek.lee@anl.gov>
2026-06-25 15:45:05 -0400

    Update the data loc

--- check_full_replay_status.py
+++ check_full_replay_status.py
@@ -2,9 +2,9 @@ from glob import glob
 import json
 import os
 
-data_dir     = "/net/cdaq/cdaql2data/cdaq/hallc-online-ndelta_vcs2_2026/ROOTfiles/"
+data_dir     = "/cache/hallc/alphaE/ndelta_vcs2_2026_online/"
 data = None
-with open('good_runs.json', "r") as file:
+with open('../hallc_replay_ndelta_vcs/vcs2_good_runs.json', "r") as file:
   data = json.load(file)
 
 for runs in data:
@@ -16,10 +16,7 @@ for runs in data:
       #print("{}".format(filesize))
       if filesize < 10000:
         print("{} run {} full replay size is too small".format(configuration, run))
-      if "c" in configuration:
-        print("{} run {} full replay exists".format(configuration, run))
-      if "e" in configuration:
-        print("{} run {} full replay exists".format(configuration, run))
+      print("{} run {} full replay exists".format(configuration, run))
     else:
       print("{} run {} full replay does not exist".format(configuration, run))
     
